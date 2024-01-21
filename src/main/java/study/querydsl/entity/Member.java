1f92768ae792148cd38a61da4e4dd0c274c2503d
wltkddn9590 <wltkddn9590@naver.com>
2024-01-22 01:40:44 +0900

    20240122 도메인 추가

---
+++ src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,41 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "mbmer_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id") // 외래 키 컬럼의 이름
+    private Team team;
+
+    public Member(String username) {
+        this(username, 0);
+    }
+
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,42 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
+@Rollback(false)
 class QuerydslApplicationTests {
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory qeury = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello result = qeury
+				.selectFrom(qHello)
+				.fetchOne();
+
+		System.out.println("result = " + result);
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
+
 	}
 
 }
